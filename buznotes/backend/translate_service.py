1fbac5bcb81c15b615397afbe99f7e72d5b0ed93
Nikita Meshcheryakov <99490790+nikitatalent@users.noreply.github.com>
2024-09-21 16:48:41 +0300

    Delete buznotes directory

--- buznotes/backend/translate_service.py
+++
@@ -1,7 +0,0 @@
-from googletrans import Translator
-
-translator = Translator()
-
-def translate_text(text, target_language):
-    translated = translator.translate(text, dest=target_language)
-    return translated.text
